d78c4bd489483a3f8a41769c314be91780b9ac80
kalex19 <klewis@rockymountainprep.org>
2018-08-11 13:09:31 +0000

    Done.

--- lib/curfew_checker.rb
+++ lib/curfew_checker.rb
@@ -1,19 +1,13 @@
-def simple_curfew_checker(time)
-  # code goes here
-end
-
-def curfew_checker(time)
-  # code goes here
-end
 
 def complex_curfew_checker(time)
-  # code goes here
+  if time >= 11 
+  puts "Go Home!"
+  elsif time == 11
+  puts "Party!"
+  else time
+  puts "It's game time!"
 end
+end 
 
-def deluxe_curfew_checker(time)
-  # code goes here
-end
 
-def platinum_curfew_checker(current_time, curfew_time)
-  # code goes here
-end
+complex_curfew_checker(12)
